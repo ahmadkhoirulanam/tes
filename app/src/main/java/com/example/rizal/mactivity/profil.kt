b91f918ccfc744ed1d0e10dd4f58fc9c10fa70fc
afrizalfahmi <kanam5999@gmail.com>
2019-04-03 18:54:17 +0700

    MoveAc

---
+++ app/src/main/java/com/example/rizal/mactivity/profil.kt
@@ -0,0 +1,12 @@
+package com.example.rizal.mactivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class profil : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
